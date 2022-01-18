9abe348ebbfaa3d64a72b5cfc7607924150b1de1
Sunil <snilavarath@gmail.com>
2022-01-18 12:27:10 -0500

    commit tree 990d33d8dc6d3dce7707625c50f60345e52eada1

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,12 @@
 import React from "react";
 import Links from "./Links";
 
-function doesBioExist(bio){
-  if (bio){
-    return <p>{bio}</p>
-  }
-    else return null
-}
+
 function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {doesBioExist(props.bio)}
+      {props.bio ? <p>{props.bio}</p> : null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.github} linkedin = {props.linkedin} />
     </div>
